0261b8388c773f5d59c8402b1f7aa954ea0f864e
0xKawaka <0xKawaka@proton.me>
2025-01-31 20:48:01 +0100

    fix button connect

--- src/components/CustomConnectButton.jsx
+++ src/components/CustomConnectButton.jsx
@@ -1,36 +1,37 @@
 import { ConnectButton } from '@rainbow-me/rainbowkit';
 
 const CustomConnectButton = ({className}) => (
-  <ConnectButton.Custom>
-    {({ account, chain, openConnectModal, openAccountModal, openChainModal, mounted }) => {
-      return (
-        <div
-          {...(!mounted && {
-            'aria-hidden': true,
-            style: { opacity: 0, pointerEvents: 'none', userSelect: 'none' }
-          })}
-        >
-          {(() => {
-            if (!mounted || !account || !chain) {
-              return (
-                <button
-                  className="className"
-                  onClick={openConnectModal}
-                >
-                  Connect Wallet
-                </button>
-              );
-            }
-            return (
-              <button className="custom-connected-button" onClick={openAccountModal}>
-                {account.displayName}
-              </button>
-            );
-          })()}
-        </div>
-      );
-    }}
-  </ConnectButton.Custom>
+  <ConnectButton />
+  // <ConnectButton.Custom>
+  //   {({ account, chain, openConnectModal, openAccountModal, openChainModal, mounted }) => {
+  //     return (
+  //       <div
+  //         {...(!mounted && {
+  //           'aria-hidden': true,
+  //           style: { opacity: 0, pointerEvents: 'none', userSelect: 'none' }
+  //         })}
+  //       >
+  //         {(() => {
+  //           if (!mounted || !account || !chain) {
+  //             return (
+  //               <button
+  //                 className="className"
+  //                 onClick={openConnectModal}
+  //               >
+  //                 Connect Wallet
+  //               </button>
+  //             );
+  //           }
+  //           return (
+  //             <button className="custom-connected-button" onClick={openAccountModal}>
+  //               {account.displayName}
+  //             </button>
+  //           );
+  //         })()}
+  //       </div>
+  //     );
+  //   }}
+  // </ConnectButton.Custom>
 );
 
 export default CustomConnectButton;
